2f7f24d04c7ceece513f1a0915450924399908b3
hanliyang <15811034090@163.com>
2018-02-03 20:59:43 +0800

    路由初始化

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const router = require('./router');
+//开放资源
 app.use('/node_modules',express.static('./node_modules/'));
 app.use('/public',express.static('./public/'));
+//绑定路由容器，使其能在app应用程序中使用
 app.use(router);
+
+
+app.listen(3000,()=>console.log("server is running on port 3000"));
+
+
+
+
--- router.js
+++ router.js
@@ -1,3 +1,39 @@
 const express = require('express');
-
+//获取路由对象
 const router = express.Router();
+
+
+//构造路由路径
+//渲染首页
+router.get('/',(req,res)=>{
+	res.send('nihao');
+
+});
+//渲染登陆页面
+router.get('/signin',(req,res)=>{
+res.send('wohao');
+
+});
+//处理登陆请求
+router.post('/signin',(req,res)=>{
+
+
+});
+//渲染注册页面
+router.get('/signup',(req,res)=>{
+
+
+});
+//处理注册请求
+router.post('/signup',(req,res)=>{
+
+
+});
+//处理退出请求
+router.get('/signout',(req,res)=>{
+
+
+});
+
+//将路由应用到app.js页面
+exports.router = router;
